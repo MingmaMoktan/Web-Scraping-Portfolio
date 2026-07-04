3388f04883f21154082d328852909743caff6355
sb13512 <sb13512@student.samk.fi>
2026-07-04 23:58:21 +0300

    Wrote a first spider that can scrape the 50 pages and 1000 books title, price and links

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -7,4 +7,20 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        books = response.css("article.product_pod")
+        for book in books:
+            yield {
+                "title": book.css("h3 a::attr(title)").get(),
+                "price": book.css("p.price_color::text").get(),
+                "link": book.css("h3 a::attr(href)").get(),
+            }
+        # This is for the pagination which means going through the different pages.
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            if 'catalogue/' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+            yield response.follow(next_page_url, callback=self.parse)
+            
+            
