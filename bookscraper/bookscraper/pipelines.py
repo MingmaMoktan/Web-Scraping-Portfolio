70d8750a6572cf543754bb41be871720dcca8d74
sb13512 <sb13512@student.samk.fi>
2026-07-24 07:17:24 +0300

    Added more processing in tthe pipelines.py

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -35,3 +35,30 @@ class BookscraperPipeline:
                 value = value.replace('£', '').strip()
                 adapter[price_key] = float(value)
         return item
+    
+        ## Availability field --> extract number of available items
+        availability_string = adapter.get('availability')
+        split_string_array = availability_string.split('(')
+        if len(split_string_array) < 2:
+            adapter['availability'] = 0
+        else:
+            availability_number_string = split_string_array[1].split(' ')
+            adapter['availability'] = int(availability_number_string[0])
+            
+        ## Review field --> convert to integer
+        num_reviews_string = adapter.get('num_reviews')
+        adapter['num_reviews'] = int(num_reviews_string)
+        
+        ## Stars field --> convert to integer
+        stars_string = adapter.get('stars')
+        split_stars_array = stars_string.split(' ')
+        stars_text_value = split_stars_array[1].lower()
+        stars_dict = {
+            'zero': 0,
+            'one': 1,
+            'two': 2,
+            'three': 3,
+            'four': 4,
+            'five': 5
+        }
+        adapter['stars'] = stars_dict.get(stars_text_value, 0)
